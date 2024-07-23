36ddd4fe818686ee80d42538479a9c232e1df1da
furkan icoz <f_icoz@hotmail.com>
2024-07-23 12:31:59 +0300

    Initial Commit

---
+++ RadioAppCleanSwiftUI/RadioAppCleanSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioAppCleanSwiftUIApp.swift
+//  RadioAppCleanSwiftUI
+//
+//  Created by Furkan ic on 23.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct RadioAppCleanSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
